5be42ddae1bcd539205af89170b693cc081f1545
thdus17 <dusghk1717@gmail.com>
2023-10-19 15:24:19 +0900

    :hammer: 무중단 배포용 shell script 추가 및 전용 디렉터리(step3)에 맞춰서 수정

---
+++ scripts/profile.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# bash는 return value가 안 되니 *제일 마지막줄에 echo로 결과 출력*후, 클라이언트에서 값 사용
+# 쉬고 있는 profile 찾기 ex: real1이 사용 중이면 real2가 쉼
+function find_idle_profile() {
+  RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
+
+  if [ ${RESPONSE_CODE} -ge 400 ] # 400보다 크면 (40x/50x 에러 포함)
+  then
+    CURRENT_PROFILE=real2
+  else
+    CURRENT_PROFILE=$(curl -s http://localhost/profile)
+  fi
+
+  if [ ${CURRENT_PROFILE} == real1 ]
+  then
+    IDLE_PROFILE=real2
+  else
+    IDLE_PROFILE=real1
+  fi
+
+  echo "${IDLE_PROFILE}"
+}
+
+# 쉬고 있는 profile의 port 찾기
+function find_idle_port() {
+    IDLE_PROFILE=$(find_idle_profile)
+
+    if [ ${IDLE_PROFILE} == real1 ]
+    then
+      echo "8082"
+    else
+      echo "8083"
+    fi
+}
