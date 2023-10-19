34d0875ce0790584e6cb7dd3d50e7c81929b7f04
thdus17 <dusghk1717@gmail.com>
2023-10-19 20:14:41 +0900

    :hammer: test code 추가

---
+++ src/test/java/com/aws/book/springboot/web/ProfileControllerTest.java
@@ -0,0 +1,27 @@
+package com.aws.book.springboot.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ProfileControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void profile은_인증없이_호출된다() {
+        String expected = "real";
+
+        ResponseEntity<String> response = restTemplate.getForEntity("/profile", String.class);
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(response.getBody()).isEqualTo(expected);
+    }
+}
